d32a3076a35d5eb36fa71ef5afacdd1d380816d6
FATIK AHMED KHAN <yousuf.hanif550@gmail.com>
2022-08-12 22:00:18 +0500

    Update App.js
    Just started calculator logics (Pending to complete full logic part)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useReducer } from "react";
 import "./style.css";
 
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
+    reducer,
+    {}
+  );
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">123123*</div>
-        <div className="current-operand">123123</div>
+        <div className="previous-operand">
+          {previousOperand}
+          {operation}
+        </div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
